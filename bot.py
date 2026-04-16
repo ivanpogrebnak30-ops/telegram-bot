5350657d8c28987883bf1209b0fc775092aafd01
ivanpogrebnak30-ops <ivanpogrebnak30@gmail.com>
2026-04-16 20:28:27 +0300

    Implement basic bot functionality with aiogram

---
+++ bot.py
@@ -0,0 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+
+bot = Bot(token="8333349750:AAFpxCIU3z5ly__Y1AhK1Dkg2f1wC7W5rCM")
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Бот работает 24/7!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if name == "main":
+    asyncio.run(main())
